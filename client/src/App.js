d61c717aa6bac344bfcede27a7253f3c6c0b9694
Lewi-Abseno <91357771+Lewi-Abseno@users.noreply.github.com>
2022-04-11 17:38:38 -0400

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,24 @@ import './App.css';
 import React from 'react'
 import Companies from './components/Companies';
 import Patient from './components/Patient';
+import Home from './components/Home';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 
 function App() {
 
   return (
-    <div>
-      <Companies />
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/Companies">Companies</Link>
+        <Link to="/Patient">Patient</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Companies" element={<Companies/>}/>
+        <Route path="/Patient" element={<Patient/>}/>
+      </Routes>
+    </Router>
   );
 }
 
